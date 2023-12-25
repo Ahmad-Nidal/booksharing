ced707ec4d43c2f02665eb18fd8f4bceccc22c6e
Ahmad Nidal <ahmadnidal.dr@gmail.com>
2023-12-26 01:12:31 +0300

    in active new account

--- booksharing/views.py
+++ booksharing/views.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.models import User
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('book_list')
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False 
+        user.save()
+        return super().form_valid(form)
